6c04a1b421739cc50684f76a157462ced783743d
Javier Donaire <jdonaire@gmail.com>
2018-11-10 22:57:36 +0100

    Improve init-minimal.el

--- init-minimal.el
+++ init-minimal.el
@@ -1,6 +1,25 @@
-(setq make-backup-files nil)
+;; Minimal emacs init file with sane (for me) defaults for quick edit
+;; sessions in slow computers without emacs-server.
+;;
+;; Example of launch script:
+;;
+;;   #!/bin/sh
+;;   exec emacs -Q -nw -l ~/.emacs.d/init-minimal.el $*
+
+(setq make-backup-files nil
+      shift-select-mode nil
+      transient-mark-mode nil
+      visible-bell t
+      initial-scratch-message nil)
+
+(prefer-coding-system 'utf-8-unix)
+(fset 'yes-or-no-p 'y-or-n-p)
+(column-number-mode t)
 (menu-bar-mode -1)
 (if (boundp 'tool-bar-mode)
     (ignore-errors (tool-bar-mode -1)))
-(fset 'yes-or-no-p 'y-or-n-p)
-(setq visible-bell t)
+
+(windmove-default-keybindings)
+
+(global-set-key (kbd "M-;") 'comment-or-uncomment-region)
+(global-set-key (kbd "M-_") 'dabbrev-expand)
